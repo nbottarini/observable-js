0ce25d4b50337787edbeeda70e6d7ca741628ce6
Nicolas Bottarini <nicolasbottarini@gmail.com>
2026-02-22 09:20:47 -0300

    add derived observable

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export type * from './ObservableValue'
 
 export { DefaultObservable, observable } from './DefaultObservable'
 export { CompositeObservable, compositeObservable } from './CompositeObservable'
+export { DerivedObservable, derivedObservable } from './DerivedObservable'
 export { ObservableMutableValue, observableValue } from './ObservableMutableValue'
 export { ObservableComputedValue, observableComputed } from './ObservableComputedValue'
